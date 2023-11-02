1957e8e0c1dfd52a6c40b45c6ec85c4904a41911
BLAZE-Official <109421416+BLAZE-Official@users.noreply.github.com>
2023-11-02 11:55:39 -0400

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'bz-fps'
+author 'BLAZE {bz}'
+description 'FPS Booster for FiveM'
+version '1.0.0'
+repository 'https://github.com/BLAZE-Official/bz-fps'
+
+client_scripts {
+    ".lua"
+}
